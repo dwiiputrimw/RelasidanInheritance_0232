ec5117a612083913bfed31ead2eb045c1e89197c
dwiiputrimw <dwiiputrimahriani@gmail.com>
2026-05-11 20:16:11 +0700

    Menambahkan class pasien

--- Asosiasi.cpp
+++ Asosiasi.cpp
@@ -4,3 +4,11 @@ using namespace std;
 
 //deklarasi class dokter
 class dokter; 
+
+//deklarasi dan pembuatan class pasien
+class pasien {
+public:
+    //deklarasi member variabel dari class pasien
+    string nama;
+    vector<dokter*> daftar_dokter;
+};
