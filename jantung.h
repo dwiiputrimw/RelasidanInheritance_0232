54c33fc3e3f33a7aea2207bd49a54f8ff4c72db8
dwiiputrimw <dwiiputrimahriani@gmail.com>
2026-05-11 20:23:44 +0700

    Menambahkan constructor destructor

--- jantung.h
+++ jantung.h
@@ -3,4 +3,14 @@
 //membuat class jantung di file header jantung.h
 class Jantung {
 public:
+    //membuat constructor dan destructor dari class jantung
+    Jantung() {
+        cout << "Jantung Ditambah\n";
+    }
+
+    ~Jantung() {
+        cout << "Jantung Dimatikan\n";
+    }
 };
+
+#endif // !JANTUNG_H
