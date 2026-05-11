659767946e6c3c023259367154eda2fff20a4d35
dwiiputrimw <dwiiputrimahriani@gmail.com>
2026-05-11 20:18:53 +0700

    Menambahkan constructor destructor & method pada class

--- Asosiasi.cpp
+++ Asosiasi.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 //deklarasi class dokter
-class dokter; 
+class dokter;
 
 //deklarasi dan pembuatan class pasien
 class pasien {
@@ -11,7 +11,21 @@ public:
     //deklarasi member variabel dari class pasien
     string nama;
     vector<dokter*> daftar_dokter;
-}
+
+    //pembuatan constructor dan destructor dari class pasien
+    pasien(string pNama) :nama(pNama) {
+        cout << "Pasien \"" << nama << "\" ada\n";
+    };
+
+    ~pasien() {
+        cout << "Pasien \"" << nama << "\" tidak ada\n";
+    };
+
+    //deklarasi prosedur tambahDokter() dan cetakDokter()
+    void tambahDoker(dokter*);
+    void cetakDokter();
+
+};
 
 //pembuatan class dokter
 class dokter {
