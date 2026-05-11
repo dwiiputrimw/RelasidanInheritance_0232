692e29a15d100594c05f9fc96d4659f702671940
dwiiputrimw <dwiiputrimahriani@gmail.com>
2026-05-11 20:28:04 +0700

    Menambahkan library dan objek manusia

--- komposisi.cpp
+++ komposisi.cpp
@@ -11,6 +11,5 @@ int main()
     //deklarasi dan pemberian nilai object pointer varManusia
     Manusia* varManusia = new Manusia("jono");
     //mendelete pointer varManusia dari memori
-    delete varManusia;
     return 0;
 }
