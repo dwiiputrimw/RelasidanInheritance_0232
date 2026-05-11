85f8524902bdfff16c87b09bf79136d12faea5da
dwiiputrimw <dwiiputrimahriani@gmail.com>
2026-05-11 20:17:08 +0700

    Menambahkan class dokter

--- Asosiasi.cpp
+++ Asosiasi.cpp
@@ -11,4 +11,12 @@ public:
     //deklarasi member variabel dari class pasien
     string nama;
     vector<dokter*> daftar_dokter;
+}
+
+//pembuatan class dokter
+class dokter {
+public:
+    //deklarasi member variabel dari class dokter
+    string nama;
+    vector<pasien*> daftar_pasien;
 };
