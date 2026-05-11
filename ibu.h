a01ca4401d5ed65a0f14440ec7236f6b9096cb79
dwiiputrimw <dwiiputrimahriani@gmail.com>
2026-05-11 20:01:49 +0700

    Menambahkan class Ibu & atributnya

--- ibu.h
+++ ibu.h
@@ -2,3 +2,10 @@
 #define IBU_H
 
 class anak;
+
+//membuat class ibu pada file header ibu.h
+class Ibu {
+    //deklarasi member variabel pada class ibu
+    string nama;
+    vector<anak*> daftar_anak;
+};
